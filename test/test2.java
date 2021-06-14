a6cb1f3900e8e46eb176bb93449e3aa02c6b0827
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2021-06-14 13:04:26 +0200

    test

---
+++ test/test2.java
@@ -0,0 +1,13 @@
+class Progrma{
+    public static void main(String[] args) {
+        test3 t = new test3();
+
+        int x = 3;
+        int y = 4;
+        System.out.println(x + y);
+
+        if (x == 3){
+            t.Count();
+        }
+    }
+}
